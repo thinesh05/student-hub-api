55592ac697f7773926d67f7fa59d0638860276a1
Thinesh <thineshtj05@gmail.com>
2024-02-02 21:03:20 +0800

    Initial commit

---
+++ database/migrations/2024_02_02_120504_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname', 128);
+            $table->char('gender', 1);
+            $table->string('email', 255);
+            $table->tinyInteger('is_active')->default(1)->comment('1=active, 0=not active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
